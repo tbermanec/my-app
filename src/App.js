f3fef713b566f2e85339eaf84bf5322c57c5bd00
Tomislav Bermanec <tbermanec@gmail.com>
2020-05-16 17:56:41 +0200

    Add basic grid

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import './App.css';
+import Typography from '@material-ui/core/Typography';
+import { Grid } from '@material-ui/core/';
+import Link from '@material-ui/core/Link';
+import Header from './components/Header';
 
 import CarsList from './components/CarsList';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <CarsList />
-    </div>
+    <Grid container direction="column">
+      <Grid item>
+        <Header />
+      </Grid>
+      <Grid>
+        <Grid item>
+          <Typography variant="h4">Available Cars</Typography>
+        </Grid>
+      </Grid>
+
+      <Grid item container>
+        <Grid item xs={false} sm={2} />
+        <Grid item xs={12} sm={8}>
+          <CarsList />
+          <Copyright />
+        </Grid>
+        <Grid item xs={false} sm={2} />
+      </Grid>
+    </Grid>
+  );
+}
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        My Car App
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
   );
 }
 
